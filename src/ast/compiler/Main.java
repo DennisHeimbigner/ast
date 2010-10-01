fd620e321aea5fb10aa5611b295b9291d8c98342
dmh <dmh@2e3060e4-1ed8-cf1f-9cc2-9586e844a556>
2010-10-01 03:59:48 +0000

    git-svn-id: https://sub.unidata.ucar.edu/common/ast/trunk@12464 2e3060e4-1ed8-cf1f-9cc2-9586e844a556

--- src/ast/compiler/Main.java
+++ src/ast/compiler/Main.java
@@ -7,21 +7,37 @@ public class Main
 {
     static public void main(String[] argv) throws Exception
     {
-	boolean pass = false;
-	FileReader rdr = new FileReader(argv[0]);
+	Getopts getopts = new Getopts("Unidata.protobuf",args,"L:d");
+        String[] arglist = getopts.argList();
+	if(arglist.length == 0) {
+	    System.err.println("No input file specified");
+	    System.exit(1);
+	}
+	FileReader rdr = new FileReader(arglist[0]);
+
         ProtobufParser parser = new ProtobufParser();
-	//parser.setDebugLevel(1);
-	pass = parser.parse(argv[0],rdr);
+	if(getopts.getSwitch('d').set()) parser.setDebugLevel(1);
+
+	boolean pass = parser.parse(arglist[0],rdr);
 	if(!pass) {
 	    System.err.println("Parse failed");
 	    System.exit(1);
 	}
+
 	// Execute Semantics
 	Semantics sem = new Semantics();
 	pass = sem.checksemantics(parser.getAST());
 	if(!pass) {
-	    System.err.println("Semantics failed");
+	    System.err.println("Semantic processing failed");
 	    System.exit(1);
 	}
+
+	// Try to locate the language specific Generator using
+	// reflection
+	String language = DEFAULTLANGUAGE;
+	if(getopts.getSwitch('l').set())
+	    language = getopts.getSwitch('l').set()) language = 
+	String
+
     }
 }
